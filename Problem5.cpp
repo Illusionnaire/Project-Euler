50954862fa3fecf055b4de4372edeeba1f27d80b
Marcin <marcinrwisniowski@gmail.com>
2019-02-14 03:58:10 -0500

    Completed Problem5, biggest number divisible by 1-n numbers. Note change product to int for precision, it is double for larger n values to not overflow

--- Problem5.cpp
+++ Problem5.cpp
@@ -1,10 +1,48 @@
-// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+/*
+    Author: Marcin Wisniowski
+*/
 #include <iostream>
+#include <vector>
 using namespace std;
 
+//Work from 2 and go upwards towards n
+//Check if number is divisible by any number within factors
+//If so, divide by that number
+//If not multiply and add number to list of factors
+
+vector<int> testFactor(int number, vector<int> factors){
+    for(int x : factors){
+        if (number % x == 0){
+            number /= x;
+        }
+    }
+    if (number > 1){
+    factors.insert(factors.begin(),number);
+    }  
+
+    return factors;
+}
+
+
 int main(){
+    int n;
+    double product = 1;
+    vector<int> factors;
     
+    cout << "Biggest number divisible by each of the numbers from 1-";
+    cin >> n;
+    
+    for (int i = 2; i <= n; i++){
+        factors = testFactor(i,factors);
+    }
+
+    for (int x : factors){
+        product *= x;
+    }
+
+    cout << "Prime factorization tree size: " << factors.size() << endl;
+    cout << product;
 
     return 0;
 }
